ae3e8e5c521a5810c67ad3f967b10c249f9834c7
Prova <prova@gmail.com>
2020-12-03 09:59:30 +0100

    Added two combobox to filter the style and the size of the product model

---
+++ WinformsUI_Project1/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WinformsUI_Project1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.subCategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.subcategoryLabel = new System.Windows.Forms.Label();
+            this.resultListView = new System.Windows.Forms.ListView();
+            this.styleComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sizeComboBox = new System.Windows.Forms.ComboBox();
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(16, 27);
+            this.categoryComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(125, 24);
+            this.categoryComboBox.TabIndex = 1;
+            this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.categoryComboBox_SelectedIndexChanged);
+            // 
+            // subCategoryComboBox
+            // 
+            this.subCategoryComboBox.FormattingEnabled = true;
+            this.subCategoryComboBox.Location = new System.Drawing.Point(168, 27);
+            this.subCategoryComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.subCategoryComboBox.Name = "subCategoryComboBox";
+            this.subCategoryComboBox.Size = new System.Drawing.Size(111, 24);
+            this.subCategoryComboBox.TabIndex = 2;
+            this.subCategoryComboBox.SelectedIndexChanged += new System.EventHandler(this.subCategoryComboBox_SelectedIndexChanged);
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(13, 7);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(63, 16);
+            this.categoryLabel.TabIndex = 4;
+            this.categoryLabel.Text = "Category";
+            // 
+            // subcategoryLabel
+            // 
+            this.subcategoryLabel.AutoSize = true;
+            this.subcategoryLabel.Location = new System.Drawing.Point(165, 7);
+            this.subcategoryLabel.Name = "subcategoryLabel";
+            this.subcategoryLabel.Size = new System.Drawing.Size(85, 16);
+            this.subcategoryLabel.TabIndex = 5;
+            this.subcategoryLabel.Text = "Subcategory";
+            // 
+            // resultListView
+            // 
+            this.resultListView.HideSelection = false;
+            this.resultListView.Location = new System.Drawing.Point(29, 139);
+            this.resultListView.Name = "resultListView";
+            this.resultListView.Size = new System.Drawing.Size(762, 354);
+            this.resultListView.TabIndex = 6;
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            this.resultListView.View = System.Windows.Forms.View.List;
+            // 
+            // styleComboBox
+            // 
+            this.styleComboBox.FormattingEnabled = true;
+            this.styleComboBox.Location = new System.Drawing.Point(312, 27);
+            this.styleComboBox.Name = "styleComboBox";
+            this.styleComboBox.Size = new System.Drawing.Size(52, 24);
+            this.styleComboBox.TabIndex = 7;
+            this.styleComboBox.SelectedIndexChanged += new System.EventHandler(this.styleComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Style";
+            // 
+            // sizeComboBox
+            // 
+            this.sizeComboBox.FormattingEnabled = true;
+            this.sizeComboBox.Location = new System.Drawing.Point(388, 27);
+            this.sizeComboBox.Name = "sizeComboBox";
+            this.sizeComboBox.Size = new System.Drawing.Size(49, 24);
+            this.sizeComboBox.TabIndex = 9;
+            this.sizeComboBox.SelectedIndexChanged += new System.EventHandler(this.sizeComboBox_SelectedIndexChanged);
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.Location = new System.Drawing.Point(388, 8);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(34, 16);
+            this.SizeLabel.TabIndex = 10;
+            this.SizeLabel.Text = "Size";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 521);
+            this.Controls.Add(this.SizeLabel);
+            this.Controls.Add(this.sizeComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.styleComboBox);
+            this.Controls.Add(this.resultListView);
+            this.Controls.Add(this.subcategoryLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.subCategoryComboBox);
+            this.Controls.Add(this.categoryComboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.ComboBox subCategoryComboBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label subcategoryLabel;
+        private System.Windows.Forms.ListView resultListView;
+        private System.Windows.Forms.ComboBox styleComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox sizeComboBox;
+        private System.Windows.Forms.Label SizeLabel;
+    }
+}
+
---
+++ WinformsUI_Project1/MainForm.cs
@@ -0,0 +1,176 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsUI_Project1
+{
+    public partial class MainForm : Form
+    {
+        private List<ProductModel> productModels;
+        public void SelectCategory()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                string catsql = "SELECT name FROM Production.ProductCategory;";
+                List<string> categories = new List<string>();
+                categories = connection.Query<string>(catsql).ToList();
+                foreach (string category in categories)
+                {
+                    categoryComboBox.Items.Add(category);
+                }
+            }
+        }
+        public void SelectSubCategory()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                string subsql = $"SELECT Production.ProductSubcategory.Name FROM Production.ProductSubcategory INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID WHERE Production.ProductCategory.Name = '{categoryComboBox.SelectedItem}' ;";
+                List<string> subcategories = new List<string>();
+                subcategories = connection.Query<string>(subsql).ToList();
+                subCategoryComboBox.Items.Clear();
+                foreach (string subcategory in subcategories)
+                {
+                    subCategoryComboBox.Items.Add(subcategory);
+                }
+            }
+        }
+        public void AddCatListView()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                productModels = new List<ProductModel>();
+                string addCatSQL = $"SELECT DISTINCT Production.ProductModel.Name, Production.ProductDescription.Description FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID INNER JOIN Production.ProductDescription ON Production.ProductDescription.ProductDescriptionID = Production.ProductModelProductDescriptionCulture.ProductDescriptionID WHERE Production.ProductModelProductDescriptionCulture.CultureID = 'en' AND Production.ProductCategory.Name = '{categoryComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                productModels = connection.Query<ProductModel>(addCatSQL).ToList();
+                resultListView.Items.Clear();
+                foreach (ProductModel productModel in productModels)
+                {
+                    resultListView.Items.Add(productModel.ToString());  
+                }       
+            }
+        }
+        public void AddSubListView()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                productModels = new List<ProductModel>();
+                string addSubSQL = $"SELECT DISTINCT Production.ProductModel.Name, Production.ProductDescription.Description FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID INNER JOIN Production.ProductDescription ON Production.ProductDescription.ProductDescriptionID = Production.ProductModelProductDescriptionCulture.ProductDescriptionID WHERE Production.ProductModelProductDescriptionCulture.CultureID = 'en' AND Production.ProductSubcategory.Name = '{subCategoryComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                productModels = connection.Query<ProductModel>(addSubSQL).ToList();
+                resultListView.Items.Clear();
+                foreach (ProductModel productModel in productModels)
+                {
+                    resultListView.Items.Add(productModel.ToString());
+                }
+            }
+        }
+
+        public void SelectStyle()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                string styleSql = $"SELECT DISTINCT Production.Product.Style FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID  INNER JOIN Production.ProductDescription ON Production.ProductModelProductDescriptionCulture.ProductDescriptionID = Production.ProductDescription.ProductDescriptionID WHERE ProductModelProductDescriptionCulture.CultureID = 'en'AND Production.ProductSubcategory.Name = '{subCategoryComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                List<string> styles = new List<string>();
+                styles = connection.Query<string>(styleSql).ToList();
+                styleComboBox.Items.Clear();
+                foreach (string style in styles)
+                {
+                    if (style == null)
+                    {
+                        styleComboBox.Enabled = false;
+                    }
+                    else
+                    {
+                        styleComboBox.Items.Add(style);
+                    }
+                }
+            }
+        }
+        public void AddStyleListView()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                productModels = new List<ProductModel>();
+                string addStSQL = $"SELECT DISTINCT Production.ProductModel.Name, Production.ProductDescription.Description FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID INNER JOIN Production.ProductDescription ON Production.ProductDescription.ProductDescriptionID = Production.ProductModelProductDescriptionCulture.ProductDescriptionID WHERE Production.ProductModelProductDescriptionCulture.CultureID = 'en' AND Production.ProductSubcategory.Name = '{subCategoryComboBox.SelectedItem}' AND Production.Product.Style = '{styleComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                productModels = connection.Query<ProductModel>(addStSQL).ToList();
+                resultListView.Items.Clear();
+                foreach (ProductModel productModel in productModels)
+                {
+                    resultListView.Items.Add(productModel.ToString());
+                }
+            }
+        }
+        public void SelecSize()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                string sizeSQL = $"SELECT DISTINCT Production.Product.Size FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID  INNER JOIN Production.ProductDescription ON Production.ProductModelProductDescriptionCulture.ProductDescriptionID = Production.ProductDescription.ProductDescriptionID WHERE ProductModelProductDescriptionCulture.CultureID = 'en'AND Production.ProductSubcategory.Name = '{subCategoryComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                List<string> sizes = new List<string>();
+                sizes = connection.Query<string>(sizeSQL).ToList();
+                sizeComboBox.Items.Clear();
+                foreach (string size in sizes)
+                { 
+                    if (size == null)
+                    {
+                        sizeComboBox.Enabled = false;
+                    }
+                    else
+                    {
+                        sizeComboBox.Items.Add(size);
+                    }
+                }
+            }
+        }
+        public void AddSizeListView()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("AdventureWorksDB")))
+            {
+                string addSizeSQL = $"SELECT DISTINCT Production.ProductModel.Name, Production.ProductDescription.Description FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID INNER JOIN Production.ProductDescription ON Production.ProductDescription.ProductDescriptionID = Production.ProductModelProductDescriptionCulture.ProductDescriptionID WHERE Production.ProductModelProductDescriptionCulture.CultureID = 'en' AND Production.ProductSubcategory.Name = '{subCategoryComboBox.SelectedItem}' AND Production.Product.Size = '{sizeComboBox.SelectedItem}' AND Product.ProductModelID IS NOT NULL";
+                productModels = new List<ProductModel>();
+                productModels = connection.Query<ProductModel>(addSizeSQL).ToList();
+                resultListView.Items.Clear();
+                foreach (ProductModel productModel in productModels)
+                {
+                    resultListView.Items.Add(productModel.ToString());
+                }
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SelectCategory();
+        }
+
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectSubCategory();
+            AddCatListView();
+        }
+
+        private void subCategoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AddSubListView();
+            SelectStyle();
+            SelecSize();
+        }
+
+        private void styleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AddStyleListView();
+        }
+
+        private void sizeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AddSizeListView();
+        }
+    }
+}
